19d8b5d159a3fc6c90997ff52190bf4bb5f332a1
Philipp Fruck <dev@p-fruck.de>
2022-03-11 09:48:09 +0100

    format polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,25 +1,38 @@
-import jsdom from 'https://dev.jspm.io/jsdom'
-import { createRequire } from "https://deno.land/std/node/module.ts";
-import { JitsiMeetJSType } from "../types/lib-jitsi-meet/JitsiMeetJS.d.ts";
+import jsdom from 'https://dev.jspm.io/jsdom';
+import { createRequire } from 'https://deno.land/std/node/module.ts';
+import { JitsiMeetJSType } from '../types/lib-jitsi-meet/JitsiMeetJS.d.ts';
+
+// require werift from node_modules, as no Deno-compatible was found yet
 const require = createRequire(import.meta.url);
-const werift = require("werift")
+(globalThis as any).require = require;
+const werift = require('werift');
 
+/**
+ * Add the missing browser API's to the Deno code.
+ *
+ * @param domain - The domain of the target jitsi instance
+ * @returns The JitsiMeetJS API object
+ */
 export async function init(domain: string): Promise<JitsiMeetJSType> {
-    // define UserAgent which is required by JitsiMeetJS
-    Object.defineProperty(navigator, 'userAgent', {
-        get: function () { return 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0)'; }
-    });
+  // define UserAgent which is required by JitsiMeetJS
+  Object.defineProperty(navigator, 'userAgent', {
+    get: function () {
+      return 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0)';
+    },
+  });
 
-    // Use jsdom to create a document element.
-    const { JSDOM } = jsdom as any;
-    var dom = new JSDOM(`<!DOCTYPE html><html><head></head><body></body></html>`);
-    (globalThis as any).document = dom.window.document
+  // use jsdom to create a document element.
+  const { JSDOM } = jsdom as any;
+  var dom = new JSDOM(`<!DOCTYPE html><html><head></head><body></body></html>`);
+  (globalThis as any).document = dom.window.document;
 
-    (globalThis as any).RTCPeerConnection = werift.RTCPeerConnection;
+  // use typescript implementation of WebRTC since this Web API is not supported natively in Deno
+  (globalThis as any).RTCPeerConnection = werift.RTCPeerConnection;
 
-    await import(`https://${domain}/libs/lib-jitsi-meet.min.js`);
-    const JitsiMeetJS: JitsiMeetJSType = (globalThis as any).JitsiMeetJS;
-    return JitsiMeetJS;
+  // dynamically import the jitsi-meet library from the given instance
+  await import(`https://${domain}/libs/lib-jitsi-meet.min.js`);
+  const JitsiMeetJS: JitsiMeetJSType = (globalThis as any).JitsiMeetJS;
+  return JitsiMeetJS;
 }
 
-export default { init }
+export default { init };
