1f7c3a223ab36110a147d898731a67a5939b2ac2
Philipp Fruck <dev@p-fruck.de>
2022-03-12 10:10:31 +0100

    add proper typings to polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,12 +1,24 @@
 import jsdom from 'https://dev.jspm.io/jsdom';
-import neoXhr from "https://dev.jspm.io/neo-xhr";
+import neoXhr from 'https://dev.jspm.io/neo-xhr';
 import xmldom from 'https://cdn.skypack.dev/@xmldom/xmldom';
 import { createRequire } from 'https://deno.land/std/node/module.ts';
 import { JitsiMeetJSType } from '../types/lib-jitsi-meet/JitsiMeetJS.d.ts';
 
+interface IGlobalPoly {
+  document: unknown;
+  require: unknown;
+  DOMParser: typeof xmldom.DOMParser;
+  JitsiMeetJS: JitsiMeetJSType;
+  RTCPeerConnection: unknown;
+  XMLHttpRequest: unknown;
+}
+
+type GlobalPolyType = typeof globalThis & IGlobalPoly;
+const globalPoly = globalThis as GlobalPolyType;
+
 // require werift from node_modules, as no Deno-compatible CDN was found yet
 const require = createRequire(import.meta.url);
-(globalThis as any).require = require;
+globalPoly.require = require;
 const werift = require('werift');
 
 /**
@@ -26,27 +38,28 @@ export async function init(instance: string): Promise<JitsiMeetJSType> {
   // use jsdom to create a document element.
   const { JSDOM } = jsdom as any;
   var dom = new JSDOM(`<!DOCTYPE html><html><head></head><body></body></html>`);
-  (globalThis as any).document = dom.window.document;
-  (globalThis as any).DOMParser = xmldom.DOMParser;
-
+  globalPoly.document = dom.window.document;
+  globalPoly.DOMParser = xmldom.DOMParser;
 
   // use typescript implementation of WebRTC since this Web API is not supported natively in Deno
-  (globalThis as any).RTCPeerConnection = werift.RTCPeerConnection;
+  globalPoly.RTCPeerConnection = werift.RTCPeerConnection;
 
   // use XMLHttpRequest polyfill, this will not be added to Deno at all, because it is legacy stuff
   const { XMLHttpRequest } = neoXhr as any;
   Object.defineProperty(XMLHttpRequest.prototype, 'responseXML', {
-    get() { return new xmldom.DOMParser().parseFromString((this as any).responseText, "text/xml"); }
+    get() {
+      return new xmldom.DOMParser().parseFromString((this as any).responseText, 'text/xml');
+    },
   });
-  (globalThis as any).XMLHttpRequest = XMLHttpRequest;
+  globalPoly.XMLHttpRequest = XMLHttpRequest;
 
   // disable console trace to reduce spam, as the jitsi xmpp strophe makes intensive use of it
-  console.trace = () => {}
+  console.trace = () => {};
 
   // dynamically import the jitsi-meet library from the given instance (requires jquery)
   await import('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
   await import(`https://${instance}/libs/lib-jitsi-meet.min.js`);
-  const JitsiMeetJS: JitsiMeetJSType = (globalThis as any).JitsiMeetJS;
+  const JitsiMeetJS: JitsiMeetJSType = globalPoly.JitsiMeetJS;
   return JitsiMeetJS;
 }
 
